2ffba29ebf2ef0484b7918b0b9789eb651cc27ec
Lucas Marohn <lukemarohn@gmail.com>
2021-10-22 12:46:58 -0700

    add fb verification

--- pages/_document.js
+++ pages/_document.js
@@ -12,6 +12,7 @@ export default class Document extends NextDocument {
           gtag('js', new Date());
           gtag('config', 'G-SVBSPMP6Z9');`}}>
         </script>
+        <meta name="facebook-domain-verification" content="sk6gv9b2jf204wlc3jdqxqvbflutu2" />
 
         </Head>
         <body style={{height: 'auto', minHeight: '100vh'}}>
